616d055ce41ec594e73c1915aacf42d1944ad7a8
Mario Carmona Parejo <mariocarmonaparejo@iescastelar.com>
2026-01-14 20:23:12 +0100

    Pruebas de ejecución - cambios en datos

--- Facturador.java
+++ Facturador.java
@@ -1,16 +1,18 @@
 public class Facturador {
+    // Tarea 3: Code Smell - Primitive Obsession (Uso de arrays en lugar de objetos)
     static String[][] repertorio = {
-        {"Tributo Robe", "heavy"}, 
-        {"Magia Knoppler", "rock"}, 
-        {"Tributo Robe", "heavy"}, 
+        {"Tributo Robe", "heavy"},
+        {"Homanaje Queen", "rock"},
+        {"Magia Knoppler", "pop"}, // Prueba 3: Provoca "Tipo de concierto desconocido"
         {"Demonios Rojos", "heavy"}
     };
 
     static Integer[][] actuaciones = {
-        {0, 2000}, 
-        {1, 1200}, 
-        {0, 950}, 
-        {3, 1140}
+        {0, 400}, // Prueba 1: Cambio a 400 asistentes
+        {2, 1200},
+        {0, 950},
+        {3, 1140},
+        {1, 800} // Prueba 2: Nueva actuación añadida
     };
 
     static String cliente = "Ayuntamiento de Badajoz";
@@ -26,25 +28,28 @@ public class Facturador {
             Integer iConcierto = actuaciones[i][0];
             Double importeActuacion = 0d;
 
+            // Tarea 3: Code Smell - Switch Statements (Lógica dependiente de tipos)
             switch (repertorio[iConcierto][1]) {
                 case "heavy":
-                    importeActuacion = 4000d;
+                    importeActuacion = 4000d; // Code Smell: Magic Number
                     if (actuaciones[i][1] > 500) {
                         importeActuacion += 20 * (actuaciones[i][1] - 500);
                     }
                     break;
                 case "rock":
-                    importeActuacion = 3000d;
+                    importeActuacion = 3000d; // Code Smell: Magic Number
                     if (actuaciones[i][1] > 1000) {
                         importeActuacion += 30 * (actuaciones[i][1] - 1000);
                     }
                     break;
                 default:
+                    // Tarea 3: Code Smell - Excepción genérica
                     throw new Exception("Tipo de concierto desconocido.");
             }
 
             totalFactura += importeActuacion;
 
+            // Tarea 3: Code Smell - Duplicación de lógica (se vuelve a evaluar el género)
             creditos += Math.max(actuaciones[i][1] - 500, 0);
             if (repertorio[iConcierto][1].equals("heavy")) {
                 creditos += actuaciones[i][1] / 5;
@@ -53,6 +58,8 @@ public class Facturador {
             System.out.println("\tConcierto: " + repertorio[iConcierto][0]);
             System.out.println("\t\tAsistentes: " + actuaciones[i][1]);
         }
+
+        // Tarea 3: Code Smell - Magic Numbers en el cálculo de impuestos
         System.out.println("BASE IMPONIBLE: " + totalFactura + " euros");
         System.out.printf("IVA (21%%): %.2f euros\n", totalFactura * 0.21);
         System.out.printf("TOTAL FACTURA: %.2f euros\n", totalFactura * 1.21);
